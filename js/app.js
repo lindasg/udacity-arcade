c1de2e34ad08849107f21d86617449f4571eef27
Yuichi Hagio <yhagio87@gmail.com>
2015-06-24 18:06:53 -0400

    Experiment: change speed. FAIL

--- js/app.js
+++ js/app.js
@@ -1,13 +1,39 @@
 var TILE_WIDTH = 101,
     TILE_HEIGHT = 83,
-    ENEMY_SPEED = 3,
-    ENEMY_NUM = 3,
+    ENEMY_SPEED = 2,
     ENEMY_X_START = -50,
     START_X_PLAYER = TILE_WIDTH * 2,
     START_Y_PLAYER = TILE_HEIGHT * 5 - 10;
 
+
+ep = document.getElementById('enemySpeed');
+ep.addEventListener('change', function(e){
+  
+  selectedEnemySpeed = ep.options[ep.selectedIndex].value;
+  allEnemies = [];
+  ENEMY_SPEED = selectedEnemySpeed - 0;
+  interval_time = auto_calc_time_push(ENEMY_SPEED);
+  pushNewEnemy(interval_time);
+
+});
+
+var auto_calc_time_push = function(enemy_speed){
+  switch (enemy_speed){
+    case 1:      
+      return 1100;
+    case 2:
+      return 700;
+    case 3:
+      return 500;
+    case 4:
+      return 500;
+    default:
+      return 1000;
+  }
+};
+
 var generate_random1_3 = function(){
-  return Math.floor(Math.random() * (3 - 1 + 1))+1;
+  return Math.floor(Math.random() * (4 - 1 + 1))+1;
 };
 
 var Enemy = function() {
@@ -18,25 +44,23 @@ var Enemy = function() {
 
 Enemy.prototype = {
   update: function(dt) {
-    this.x += (dt * 100 * ENEMY_SPEED);
+    this.x += (dt * 100 * (ENEMY_SPEED+1));
+
     if((player.x == this.x)){
       player.x = START_X_PLAYER;
       player.y = START_Y_PLAYER;
     }
 
     if(this.x > TILE_WIDTH * 5){
-      this.x = ENEMY_X_START;
-      this.y = 83 * generate_random1_3() - 20;
-    }
-
-    if(allEnemies.length >= ENEMY_NUM){
-      clearInterval(newEnemies);
+      var index = allEnemies.indexOf(this);
+      allEnemies.splice(index, 1);
     }
 
     if((player.x - this.x < 30) && (this.x - player.x < 30) && (this.y - player.y < 30) && (player.y - this.y < 30)){
       player.x = START_X_PLAYER;
       player.y = START_Y_PLAYER;
     }
+
     
   },
 
@@ -93,15 +117,17 @@ Player.prototype = {
 };
 
 
-
 var player = new Player();
 var allEnemies = [];
+var interval_time;
 
-var newEnemies = setInterval(function(){
-  allEnemies.push(new Enemy());
-}, 1200);
-
-newEnemies;
+allEnemies.push(new Enemy());
+var pushNewEnemy = function(interval_time){
+  setInterval(function(){
+    allEnemies.push(new Enemy());
+  }, interval_time);
+};
+pushNewEnemy(auto_calc_time_push(ENEMY_SPEED));
 
 
 
