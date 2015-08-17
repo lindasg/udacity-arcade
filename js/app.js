7ffd6e69fcbcd60b57ca556f27213212c63517e6
Yuichi Hagio <yhagio87@gmail.com>
2015-08-17 19:32:03 -0400

    No 4 spaces. 1 space between ) { in function. redirect URL fixed.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 // Variables to be used in the game
-var TILE_WIDTH = 101,
-    TILE_HEIGHT = 83,
-    ENEMY_SPEED = 3,
-    ENEMY_X_START = -50,
-    START_X_PLAYER = TILE_WIDTH * 4,
-    START_Y_PLAYER = TILE_HEIGHT * 5 - 10,
-    TIME_INTERVAL = 500,
-    POINT = 3;
+var TILE_WIDTH = 101;
+var TILE_HEIGHT = 83;
+var ENEMY_SPEED = 3;
+var ENEMY_X_START = -50;
+var START_X_PLAYER = TILE_WIDTH * 4;
+var START_Y_PLAYER = TILE_HEIGHT * 5 - 10;
+var TIME_INTERVAL = 500;
+var POINT = 3;
 
 // Generate Random Y Axis (between 1st stone tile to 4th grass tile from top)
-function randomYPosition(){
+function randomYPosition() {
   return Math.floor(Math.random() * (4 - 1 + 1))+1;
 }
 
 // Generate Random X Axis (between 1st stone tile to 10th from left)
-function randomXPosition(){
+function randomXPosition() {
   return Math.floor(Math.random() * (9 - 0 + 1));
 }
 
 // Overwrite inner HTML text of target id element
-function insertTextOfElement(idName, insertText){
+function insertTextOfElement(idName, insertText) {
   return document.getElementById(idName).innerHTML = insertText;
 }
 
 // Set player's position to starting point
-function setPlayerStartPosition(player){
+function setPlayerStartPosition(player) {
   player.x = START_X_PLAYER;
   player.y = START_Y_PLAYER;
 }
@@ -42,21 +42,21 @@ Enemy.prototype = {
     this.x += (dt * 100 * (ENEMY_SPEED+1));
 
     // Delete enemy once it crosses the canvas
-    if(this.x > TILE_WIDTH * 10){
+    if(this.x > TILE_WIDTH * 10) {
       var index = allEnemies.indexOf(this);
       allEnemies.splice(index, 1);
     }
 
     // When enemy hits player (note: consider the size of graphics)
-    if(player){
-      if((player.x - this.x < 30) && (this.x - player.x < 30) && (this.y - player.y < 30) && (player.y - this.y < 30)){
+    if(player) {
+      if((player.x - this.x < 30) && (this.x - player.x < 30) && (this.y - player.y < 30) && (player.y - this.y < 30)) {
         setPlayerStartPosition(player);
-        if(POINT >= 1){
+        if(POINT >= 1) {
           POINT--;
         }
         insertTextOfElement('points', POINT);
 
-        if(POINT === 0){
+        if(POINT === 0) {
           popUpMessage();
         }
       }
@@ -70,7 +70,7 @@ Enemy.prototype = {
 };
 
 // Generate player on starting position
-var Player = function(){
+var Player = function() {
   this.sprite = 'images/char-boy.png';
   setPlayerStartPosition(this);
 };
@@ -78,30 +78,30 @@ var Player = function(){
 // Define Player object
 Player.prototype = {
   // Set player position
-  render: function(){
+  render: function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   },
 
   // Handle player key moves
-  handleInput: function(key){
-    switch (key){
+  handleInput: function(key) {
+    switch (key) {
       case 'up':
-        if(this.y > -10){
+        if(this.y > -10) {
           this.y -= TILE_HEIGHT;
         }
         break;
       case 'down':
-        if(this.y < START_Y_PLAYER){
+        if(this.y < START_Y_PLAYER) {
           this.y += TILE_HEIGHT;
         }
         break;
       case 'right':
-        if(this.x < 101*9){
+        if(this.x < 101*9) {
           this.x += TILE_WIDTH;
         }
         break;
       case 'left':
-        if(this.x > 0){
+        if(this.x > 0) {
           this.x -= TILE_WIDTH;
         }
         break;
@@ -110,28 +110,28 @@ Player.prototype = {
 };
 
 // Heart item
-var Heart = function(){
+var Heart = function() {
   this.sprite = 'images/Heart.png';
   this.x = TILE_WIDTH * randomXPosition();
   this.y = TILE_HEIGHT-20;
 };
 
 Heart.prototype = {
-  render: function(){
+  render: function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   },
 
   // Randomly place a new heart after user collects once
   // also update points
-  update: function(){
-    if((player.x - this.x < 30) && (this.x - player.x < 30) && (this.y - player.y < 30) && (player.y - this.y < 30)){
+  update: function() {
+    if((player.x - this.x < 30) && (this.x - player.x < 30) && (this.y - player.y < 30) && (player.y - this.y < 30)) {
       POINT+=2;
       setPlayerStartPosition(player);
       insertTextOfElement('points', POINT);
       this.x = TILE_WIDTH * randomXPosition();
 
       // Pop up message on reaching 10 points
-      if(POINT >= 10){
+      if(POINT >= 10) {
         popUpMessage();
       }
     }
@@ -139,28 +139,28 @@ Heart.prototype = {
 };
 
 // Star item
-var Star = function(){
+var Star = function() {
   this.sprite = 'images/Star.png';
   this.x = TILE_WIDTH * randomXPosition();
   this.y = TILE_HEIGHT * 2 -20;
 };
 
 Star.prototype = {
-  render: function(){
+  render: function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   },
 
   // Randomly place a new star after user collects once
   // also update points
-  update: function(){
-    if((player.x - this.x < 30) && (this.x - player.x < 30) && (this.y - player.y < 30) && (player.y - this.y < 30)){
+  update: function() {
+    if((player.x - this.x < 30) && (this.x - player.x < 30) && (this.y - player.y < 30) && (player.y - this.y < 30)) {
       POINT++;
       setPlayerStartPosition(player);
       insertTextOfElement('points', POINT);
       this.x = TILE_WIDTH * randomXPosition();
 
       // Pop up message on reaching 10 points
-      if(POINT >= 10){
+      if(POINT >= 10) {
         popUpMessage();
       }
     }
@@ -171,14 +171,14 @@ Star.prototype = {
 var player, allEnemies, enemyCreation, heart, star;
 
 // Generate a new enemy and push it to the enemy array
-function enemyGenerationCycle(){
-  enemyCreation = setInterval(function(){
+function enemyGenerationCycle() {
+  enemyCreation = setInterval(function() {
     allEnemies.push(new Enemy());
   }, TIME_INTERVAL);
 }
 
 // Initialize a new game
-function startGame(){
+function startGame() {
   clearInterval(enemyCreation);
   player = new Player();
   heart = new Heart();
@@ -190,13 +190,13 @@ function startGame(){
 
 // Pop up message on reaching 10 points or losing the game
 // and reset the points to 3
-function popUpMessage(){
-  if(POINT === 0){
-    window.location.href = 'http://localhost:8080/#gameover';
+function popUpMessage() {
+  if(POINT === 0) {
+    window.location.href = '/#gameover';
     POINT = 3;
     insertTextOfElement('points', POINT);
-  } else if(POINT >= 10){
-    window.location.href = 'http://localhost:8080/#accomplished';
+  } else if(POINT >= 10) {
+    window.location.href = '/#accomplished';
     POINT = 3;
     insertTextOfElement('points', POINT);
   }
